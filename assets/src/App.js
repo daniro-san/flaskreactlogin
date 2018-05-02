4f0077eb05a06d1b4b6d785d39ca479ff2dad607
daniro-san <danilao.rs@gmail.com>
2018-05-02 11:41:15 -0300

    requirements.txt added

--- assets/src/App.js
+++ assets/src/App.js
@@ -20,7 +20,6 @@ class App extends React.Component {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({
         "name": user,
