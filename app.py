0c92cb3ec1a70a0187dd3f7b168823c7191d96b5
daniro-san <danilao.rs@gmail.com>
2018-05-02 15:23:38 -0300

    update app.py

--- app.py
+++ app.py
@@ -43,5 +43,4 @@ def register():
   return jsonify({'status' : -1, 'message': 'username already registered, try using another'})
 
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
-  app.run(debug=True,port=port)
+  app.run(debug=True)
